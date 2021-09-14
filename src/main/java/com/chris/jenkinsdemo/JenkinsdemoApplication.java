6a5ad2bc8a0a18b9a795cfcca37a84f95d3eb86b
chrismathew <cmathew06@gmail.com>
2021-09-14 15:54:28 -0400

    compile error fix

--- src/main/java/com/chris/jenkinsdemo/JenkinsdemoApplication.java
+++ src/main/java/com/chris/jenkinsdemo/JenkinsdemoApplication.java
@@ -1,7 +1,11 @@
 package com.chris.jenkinsdemo;
 
+import com.hazelcast.client.config.ClientConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JenkinsdemoApplication {
